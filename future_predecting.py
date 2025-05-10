76a558d9c159f3c9ecbe3448d89f297fe1bad143
anhadbatra <anhadbatra1998@gmail.com>
2025-05-10 13:43:32 -0400

    future_predictions

--- future_predecting.py
+++ future_predecting.py
@@ -2,33 +2,41 @@ import tensorflow as tf
 import pandas as pd
 import awswrangler as wr
 from sklearn.preprocessing import MinMaxScaler
+import matplotlib.pyplot as plt
+from tensorflow.python.keras.models import Sequential, load_model
+from tensorflow.python.keras.layers import LSTM, Dense
+
+
 
 def get_raw_data(stock_name):
     # Replace with your bucket and path
     s3_path = f"s3://financialdatastocks/{stock_name}.csv"
-    print(f"Fetching data from {s3_path}")
     df = wr.s3.read_csv(s3_path)
     df['timestamp'] = pd.to_datetime(df['timestamp'])  # Ensure timestamp is in datetime format
     return df
 
-stocks = ['IBM', 'AAPL']
-for stock in stocks:
-    df_data = get_raw_data(stock)
+def main():
+    stocks = ['IBM', 'AAPL']
+    for stock in stocks:
+        df_data = get_raw_data(stock)
 
     # Split data into training and testing sets
-    mid_prices = (df_data['high'] + df_data['low']) / 2
-    train_data = mid_prices[int(len(mid_prices) * 0.8)]
-    test_data = mid_prices[int(len(mid_prices) * 0.2):]
+        high_prices = df_data.loc[:,'2. high'].values
+        low_prices = df_data.loc[:,'3. low'].values
+        scaler = MinMaxScaler()
+        high_prices_scaled = scaler.fit_transform(high_prices.reshape(-1,1)) # this expects numpy array
+        low_prices_scaled = scaler.fit_transform(low_prices.reshape(-1,1))
+        split = int(0.8 * len(high_prices_scaled))
+        high_price_train, high_price_test = high_prices_scaled[:split], high_prices_scaled[split:]
+        low_price_train, low_price_test = low_prices_scaled[:split], low_prices_scaled[split:]
+        model = Sequential()
+        
 
-    scaler = MinMaxScaler()
-    train_data = train_data.reshape(-1, 1)
-    test_data = test_data.reshape(-1, 1)
 
-    EMA = 0.0
-    gamma = 0.1
-    for ti in range(len(train_data)):
-        EMA = gamma * train_data[ti] + (1 - gamma) * EMA
-        train_data[ti] = EMA
+
+if __name__ == '__main__':
+    main()
+
 
 
 
