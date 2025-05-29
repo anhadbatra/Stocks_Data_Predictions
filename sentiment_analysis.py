bca7a2f788256fb64a8102a55d8c3971594deba4
anhadbatra <anhadbatra1998@gmail.com>
2025-05-29 00:27:20 -0400

    snowflake done

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -2,9 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from textblob import TextBlob
-import csv
+import os
 import datetime
-from pyhive import hive
+import snowflake.connector
 r = requests.get("https://appleinsider.com/")
 
 
@@ -25,13 +25,38 @@ date_now = datetime.date.today()
 data = [
     date_now,polarity,vander_scores,positive_counter,negative_counter
 ]
-conn = hive.Connection(
-    host='localhost'
-    port=10000
-    database='stock_prices'
-    password=''
+conn = snowflake.connector.connect(user = os.environ.get('SNOWFLAKE_USER'),
+                                    password = os.environ.get('SNOWFLAKE_PASSWORD'),
+                                    account = os.environ.get('SNOWFLAKE_ACCOUNT'),
+                                    warehouse = os.environ.get('SNOWFLAKE_WAREHOUSE'),
+                                    database = os.environ.get('SNOWFLAKE_DATABASE'),
+                                    schema = os.environ.get('SNOWFLAKE_SCHEMA')
+
 
 )
+cursor = conn.cursor()
+insert_query = """
+INSERT INTO sentiment_data (
+    date, textblob_polarity, vader_neg, vader_neu, vader_pos, vader_compound,
+    positive_mentions, negative_mentions
+)
+VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+"""
+
+cursor.execute(insert_query, (
+    date_now,
+    polarity,
+    vander_scores['neg'],
+    vander_scores['neu'],
+    vander_scores['pos'],
+    vander_scores['compound'],
+    positive_counter,
+    negative_counter
+))
+
+conn.commit()
+cursor.close()
+conn.close()
 
 
 
