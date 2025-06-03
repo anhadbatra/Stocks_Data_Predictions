7e166df1fb7b3c6450d4865e8e69670cc80e3594
anhadbatra <anhadbatra1998@gmail.com>
2025-06-02 21:57:26 -0400

    refactoring

--- dags/main.py
+++ dags/main.py
@@ -1,6 +1,6 @@
 from airflow import DAG
 from datetime import datetime
-from .get_data import get_data
+from ..get_data import get_data
 from airflow.operators.python import PythonOperator
 
 with DAG("Stock_Data",start_date=datetime(2025,5,31),schedule_interval='0 21 * * 1,3,5',catchup=False) as dag:
