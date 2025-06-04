89d0bf5fa69b94e5b475171f76308226ceb28dc3
anhadbatra <anhadbatra1998@gmail.com>
2025-06-03 23:28:50 -0400

    work_updated

--- app/model.py
+++ app/model.py
@@ -18,36 +18,47 @@ def connection():
                                     warehouse = os.environ.get('SNOWFLAKE_WAREHOUSE'),
                                     database = os.environ.get('SNOWFLAKE_DATABASE'),
                                     schema = os.environ.get('SNOWFLAKE_SCHEMA')
-
-
+    
+    
 )
+    return conn.cursor()
 
-def create_sequences(data, seq_length):
+def get_data():
+    cursor = connection()
+    query = """
+    SELECT * FROM STOCK_DATA_DW LIMIT 30;
+"""
+    data = cursor.execute(query)
+    
+def create_sequences(features,target, seq_length):
     X, y = [], []
-    for i in range(len(data) - seq_length):
-        X.append(data[i:i + seq_length])
-        y.append(data[i + seq_length])
+    for i in range(len(features) - seq_length):
+        X.append(features[i:i + seq_length])
+        y.append(target[i + seq_length])
     return np.array(X), np.array(y)
 
-def premilinary_analysis():
-        df_data = get_raw_data(stock)
+def preliminary_analysis():
+    df_data = get_data()
+    feature_columns = ['OPEN', 'HIGH', 'LOW', 'CLOSE', 'VOLUME', 'POLARITY',
+                       'COMPOUND', 'POS', 'NEU', 'NEG', 'POSITIVE_KEYWORDS', 'NEGATIVE_KEYWORDS']
+    features = df_data[feature_columns]
+    target = df_data['CLOSE']
 
-    # Split data into training and testing sets
-        timestamps = df_data['timestamp'].values
-        high_prices = df_data.loc[:,'2. high'].values
-        low_prices = df_data.loc[:,'3. low'].values
-        scaler = MinMaxScaler()
-        high_prices_scaled = scaler.fit_transform(high_prices.reshape(-1,1))
-        low_prices_scaled = scaler.fit_transform(low_prices.reshape(-1,1))
-        split = int(0.8 * len(high_prices_scaled))
-        high_price_train, high_price_test = high_prices_scaled[:split], high_prices_scaled[split:]
-        low_price_train, low_price_test = low_prices_scaled[:split], low_prices_scaled[split:]
-        timestamp_train, timestamp_test = timestamps[:split], timestamps[split:]
-        return high_price_train,high_price_test,low_price_train,low_price_test,timestamp_test,timestamp_train
+    scaler = MinMaxScaler()
+    scaled_data = scaler.fit_transform(features)
+    scaled_target = scaler.fit_transform(features['CLOSE'])
+    X,y = create_sequences(scaled_data,scaled_target,seq_length=10)
+    split = int(0.8 * len(df_data))
+    X_train = X[:split]
+    X_test = X[split:]
+    y_train = y[:split]
+    y_test = y[split:]
+    return X_train,X_test, y_train,y_test
+
         
-def define_model(high_price_train):
+def define_model(df_train):
     model = Sequential()
-    model.add(LSTM(50,return_sequences=True,input_shape=(high_price_train.shape[1],1)))
+    model.add(LSTM(50,return_sequences=True,input_shape=df_train.shape[1]))
     model.add(Dropout(0.2))
     model.add(LSTM(50))
     model.add(Dropout(0.2))
@@ -55,15 +66,15 @@ def define_model(high_price_train):
     model = model.compile(optimizer='adam',metrics=['accuracy'])
     return model
 
-def fit_model(model,high_price_train,low_price_train,scaler):
-    model.fit(high_price_train,low_price_train,epochs=5,batch_size=32)
-    predictions = model.predict(high_price_test)
+def fit_model(model,X_train,y_train,X_test,y_test,scaler):
+    model.fit(X_train,y_train,epochs=5,batch_size=32)
+    predictions = model.predict()
     predictions = scaler.inverse_transform(predictions)
-    actual = scaler.inverse_transform(low_price_test)
+    actual = scaler.inverse_transform(y_test)
     mse = mean_squared_error(actual, predictions)
-    return mse
+    return predictions , mse
 
-class tcn_model():
+class tcn_model:
     def __init__(self):
         model = Sequential([
             Input(shape=(10,1)),
@@ -73,6 +84,11 @@ class tcn_model():
         ]
         )
         model.compile(optimizer='adam',loss='mse')
+    def fit(self,model,X,y):
+        self.model.fit(X,y,epochs=5,batch_size=32)
+    def predict(self,X):
+        
+
         
         
         
@@ -84,7 +100,7 @@ class tcn_model():
 
 
 if __name__ == '__main__':
-    get_raw_data()
+    g
 
 
 
