cf0bc661aef106053837c5395c43818affafb3ea
anhadbatra <anhadbatra1998@gmail.com>
2025-06-15 23:56:43 -0400

    fast_apo

--- app/main.py
+++ app/main.py
@@ -1,3 +1,14 @@
 from fastapi import FastAPI
-from app import model
+from app.model import predict_lstm
+from fastapi.responses import JSONResponse
 
+app = FastAPI(title="Stock Prediction API")
+
+@app.get("/")
+def root():
+    return {"message": "Welcome to the Stock Prediction API"}
+
+@app.get("/predict-lstm")
+def get_lstm_prediction():
+    predictions, mse = predict_lstm()
+    return JSONResponse(content={"predictions": predictions, "mse": mse})
